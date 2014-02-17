496410c79f17fb8aeb1ba69b9642a657299a43bd
Sandro Mello <sandromll@gmail.com>
2014-02-17 15:20:32 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 rm -f ~/.vim/colors/molokai.vim
 rm -f ~/.vimrc
-wget https://raw2.github.com/sandromello/.vimrc/master/.vimrc 
+wget --no-check-certificate https://raw2.github.com/sandromello/.vimrc/master/.vimrc 
 mkdir -p ~/.vim/colors
 cd ~/.vim/colors
-wget https://raw2.github.com/sandromello/.vimrc/master/molokai.vim
+wget --no-check-certificate https://raw2.github.com/sandromello/.vimrc/master/molokai.vim
